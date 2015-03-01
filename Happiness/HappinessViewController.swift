3664adbda8ea9f2ddf1b7e39c017e57e63b8961f
kolesnikovakate <kolesnikovakate@gmail.com>
2015-03-01 21:51:42 +0300

    Урок 5, Улыбающиеся/грустное лицо

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Екатерина Колесникова on 01.03.15.
+//  Copyright (c) 2015 kkate. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CS193P_Happiness
-Developing iOS 8 Apps with Swift, Stanford CS193p
